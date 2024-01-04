39f55fca75fd4b9f29830e19f84d8177a09eff72
Tomas Kello <tomas.kello@pdisoftware.com>
2024-01-04 22:41:05 +0100

    Add project files.

---
+++ EmproverAPI/Controllers/Market_ChartController.cs
@@ -0,0 +1,84 @@
+﻿using EmproverAPI.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmproverAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class Market_ChartController : Controller
+    {
+        private readonly UserContext _context;
+
+        public Market_ChartController(UserContext userContext)
+        {
+            _context = userContext;
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            return Ok(_context.Users);
+        }
+
+        [HttpGet]
+        public ActionResult GetAdmin()
+        {
+            return Ok(_context.Users.Where(u => u.Permissions == UserPermissions.Admin));
+        }
+
+        [HttpPost]
+        public ActionResult AddIndicator([FromBody] Indicator indicator)
+        {
+            _context.Indicators.Add(indicator);
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult AddSymbol([FromBody] Symbol symbol)
+        {
+            _context.Symbols.Add(symbol);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost("symbolCode")]
+        public async Task<ActionResult> AddData(string symbolCode, [FromBody] List<DayStatisticsDto> data)
+        {
+            var symbol = await _context.Symbols
+                                .Include(s => s.DayStatistics)
+                                .ThenInclude(s => s.Point)
+                                .FirstOrDefaultAsync(s => s.Code == symbolCode);
+
+            if (symbol == null)
+            {
+                return NotFound($"Symbol with code {symbolCode} not found.");
+            }
+
+            foreach (var dayStatisticsDto in data)
+            {
+                var dayStatistics = new DayStatistics
+                {
+                    Kapitalizacia = dayStatisticsDto.Kapitalizacia,
+                    BuySellVolume = dayStatisticsDto.BuySellVolume,
+                    Point = new Point
+                    {
+                        DateTime = dayStatisticsDto.Point.DateTime,
+                        OpenValue = dayStatisticsDto.Point.OpenValue,
+                        CloseValue = dayStatisticsDto.Point.CloseValue,
+                        MinValue = dayStatisticsDto.Point.MinValue,
+                        MaxValue = dayStatisticsDto.Point.MaxValue
+                    }
+                };
+
+                symbol.DayStatistics.Add(dayStatistics);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok("DayStatistics added successfully");
+        }
+    }
+}
---
+++ EmproverAPI/Models/Class.cs
@@ -0,0 +1,148 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EmproverAPI.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Symbol> Symbols { get; set; }
+        public DbSet<DayStatistics> DayStatistics { get; set; }
+        public DbSet<Indicator> Indicators { get; set; }
+        public DbSet<IndicatorParameter> IndicatorParameter { get; set; }
+        public DbSet<AllowedValues> AllowedValues { get; set; }
+    }
+    public class Point
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public int OpenValue { get; set; }
+        public int CloseValue { get; set; }
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+    }
+    public class PointDto
+    {
+        public DateTime DateTime { get; set; }
+        public int OpenValue { get; set; }
+        public int CloseValue { get; set; }
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+    }
+
+    public class Symbol
+    {
+        [Key]
+        [StringLength(15)]
+        public string Code { get; set; } = string.Empty;
+        [StringLength(15)]
+        public string Name { get; set; } = string.Empty; // can be EUR/USD, Gold, Silver, Cupprum
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public List<DayStatistics> DayStatistics { get; set; }
+    }
+
+    public class DayStatistics
+    {
+        [Key]
+        public int Id { get; set; }
+        public Point Point { get; set; } = new Point();
+        public int Kapitalizacia { get; set; }
+        public int BuySellVolume { get; set; }
+        //public object? Obj1 { get; set; }
+        //public object? Obj2 { get; set; }
+        //public object? Obj3 { get; set; }
+        //public object? Obj4 { get; set; }
+        //public object? Obj5 { get; set; }
+    }
+
+    public class DayStatisticsDto
+    {
+        public PointDto Point { get; set; } = new PointDto();
+        public int Kapitalizacia { get; set; }
+        public int BuySellVolume { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        [StringLength(15)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(150)]
+        public string Description { get; set; } = string.Empty;
+        [StringLength(15)]
+        public string Password { get; set; } = string.Empty;
+        public UserPermissions Permissions { get; set; }
+    }
+
+    public enum UserPermissions
+    {
+        Admin,
+        Trader,
+        User
+    }
+
+    public class Indicator
+    {
+        [Key]
+        [StringLength(15)]
+        public string Code { get; set; } = string.Empty;
+        [StringLength(15)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(150)]
+        public string Description { get; set; } = string.Empty;
+        public List<IndicatorParameter> IndicatorParameters { get; set; } = new List<IndicatorParameter> { };
+        public IndicatorDisplayType DisplayType { get; set; }
+    }
+
+    public class IndicatorParameter
+    {
+        [Key]
+        [StringLength(15)]
+        public string Name { get; set; } = string.Empty;
+        public int Index { get; set; }
+        public IndicatorDisplayType DisplayType { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? DefaultValue { get; set; }
+        public List<AllowedValues> AllowedValues { get; set; } = new List<AllowedValues>();
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? MinValue { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? MaxValue { get; set; }
+        public ParameterType ParameterType { get; set; }
+        //public FunctionToDo Fce { get; set; } = new FunctionToDo();
+    }
+
+    public class AllowedValues
+    {
+        [Key]
+        public int Key { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Value { get; set; }
+    }
+
+    //public class FunctionToDo
+    //{
+    //}
+
+    public enum ParameterType
+    {
+        TypeInt,
+        TypeDecimal
+    }
+    public enum IndicatorDisplayType
+    {
+        Line,
+        Spot
+    }
+}
